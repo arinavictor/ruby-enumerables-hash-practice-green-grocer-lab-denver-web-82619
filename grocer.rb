0b5afac915bc654982072054aa0d32252c2f2229
Arina Victor <arinavictor@yahoo.com>
2019-08-22 00:44:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,23 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  cart.each do |items_hash|
+    items_hash.each do |item, attribute_hash|
+      new_cart[item] ||= attribute_hash
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :
+      new_cart[item][:count] = 1 
+    end
+  end
+  
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |coupon|
+    coupon.each do |attribute, value|
+      name = coupon[:item]
+  if 
+  
 end
 
 def apply_clearance(cart)
