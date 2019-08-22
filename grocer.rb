b3f9b4b32c22146d2f60f297d3aa66b92d905285
Arina Victor <arinavictor@yahoo.com>
2019-08-22 01:00:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     coupon.each do |attribute, value|
       name = coupon[:item]
-  if cart
+  if cart["#{name} W/ COUPON"]
   
 end
 
